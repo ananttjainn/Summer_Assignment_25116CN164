b93439cd1136fc0925c1985a4d56e4de07ca37ca
ananttjainn <anantjain5351@gmail.com>
2026-06-06 22:25:26 +0530

    Day6 Uploaded.

---
+++ Day6/24_question.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int x, n, result = 1;
+    printf("Enter Base = ");
+    scanf("%d", &x);
+    printf("Enter Power = ");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        result = result * x;
+    }
+    printf("Answer = %d\n", result);
+    return 0;
+}
