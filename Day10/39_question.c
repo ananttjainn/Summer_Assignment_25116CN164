59fafbcef3b96d9d6de26c20a989e3c6b8932807
ananttjainn <anantjain5351@gmail.com>
2026-06-10 23:23:51 +0530

    Day10 Uploaded.

---
+++ Day10/39_question.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    for(a = 1; a <= 5; a++)
+    {
+        for(b = 1; b <= 5 - a; b++)
+        printf(" ");
+        for(c = 1; c <= a; c++)
+        printf("%d", c);
+        for(c = a - 1; c >= 1; c--)
+        printf("%d", c);
+        printf("\n");
+    }
+    return 0;
+}
